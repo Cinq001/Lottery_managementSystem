54c1f614d65ad90c6115f989ae9132d03e02d4f3
chenwei <cinq19960127@gmail.com>
2018-03-31 14:54:25 +0800

    彩票管理系统

---
+++ main_test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(void)
+{
+    int x;
+    cout<<"请输入ｘ的值:"<<endl;
+    cin>>x;
+    cout<<"你输入的ｘ的值为："<<x;
+    cout<<endl;
+    cout<<"Hello,world!"<<endl;
+    return 0;
+}
